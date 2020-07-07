5b5b0a68815e9d92fd32fcaf50186f1667cbc668
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 11:36:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,7 @@ end
 def turn_count(board=[" "," "," "," "," "," "," "," "," "])
   counter=0
   board.each do |input|
-    if input=="X"
+    if (input=="X")||(input=="O")
       counter+=1
     end
   end
