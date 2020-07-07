82552c53b29be5588a5350166032aaebec3798d1
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 11:31:45 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,17 @@
-def current_player(board)
+def current_player(board=[" "," "," "," "," "," "," "," "," "])
   counter=turn_count(board)
   if counter%2==0
     return "X"
   else
     return "O"
+  end
+end
+
+def turn_count(board=[" "," "," "," "," "," "," "," "," "])
+  counter=0
+  board.each do |input|
+    if input=="X"
+      counter+=1
+    end
   end
 end
